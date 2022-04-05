f88b39ba4c6003dd44cdebeb1070a9ca4bafd661
jazibghori <77227927+jazibghori@users.noreply.github.com>
2022-04-05 14:30:11 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    music.changeTempoBy(-5)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(music.tempo())
+})
+input.onButtonPressed(Button.B, function () {
+    music.changeTempoBy(5)
+})
+music.setTempo(100)
+basic.forever(function () {
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+})
